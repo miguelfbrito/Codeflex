5ecfca9ac99e11b738ddece5fc8d7980e545bbb4
Miguel Brito <miguelfbrito11@gmail.com>
2018-04-15 23:54:47 +0100

    Added C++, C# and Python to the list of possible languages used

--- codeflex/src/main/java/pt/codeflex/controllers/CompilerController.java
+++ codeflex/src/main/java/pt/codeflex/controllers/CompilerController.java
@@ -19,54 +19,80 @@ import net.schmizz.sshj.xfer.FileSystemFile;
 @Controller
 public class CompilerController {
 
-	
 	@GetMapping("/compiler")
 	public String compiler() {
 		return "compiler";
 	}
 
-	public EvaluateSubmissions evaluateSubmissions;
-
 	@GetMapping("/ssh")
 	public String ssh() throws IOException {
-		evaluateSubmissions = new EvaluateSubmissions();
-		evaluateSubmissions.connect();
-		evaluateSubmissions.compileAndRun("JAVA", "import java.io.*;\r\n" + 
-				"import java.util.*;\r\n" + 
-				"import java.text.*;\r\n" + 
-				"import java.math.*;\r\n" + 
-				"import java.util.regex.*;\r\n" + 
-				"\r\n" + 
-				"public class Solution {\r\n" + 
-				"\r\n" + 
-				"	public static void main(String[] args) {\r\n" + 
-				"		Scanner in = new Scanner(System.in);\r\n" + 
-				"		int n = in.nextInt();\r\n" + 
-				"		int scores[] = new int[n];\r\n" + 
-				"		for (int i = 0; i < n; i++) {\r\n" + 
-				"			scores[i] = in.nextInt();\r\n" + 
-				"		}\r\n" + 
-				"		minimumDistances(n, scores);\r\n" + 
-				"	}\r\n" + 
-				"\r\n" + 
-				"	static void minimumDistances(int n, int array[]) {\r\n" + 
-				"		int min = Integer.MAX_VALUE;\r\n" + 
-				"		for (int i = 0; i < n-1; i++) {\r\n" + 
-				"			for(int j = i+1; j<n; j++) {\r\n" + 
-				"				if(array[i] == array[j]){\r\n" + 
-				"					min = Math.min(min,  Math.abs(i-j));\r\n" + 
-				"				}\r\n" + 
-				"			}\r\n" + 
-				"		}\r\n" + 
-				"		if(min==Integer.MAX_VALUE) {\r\n" + 
-				"			min = -1;\r\n" + 
-				"		}\r\n" + 
-				"		System.out.println(min);\r\n" + 
-				"	}\r\n" + 
-				"\r\n" + 
-				"\r\n" + 
-				"}", "input.txt", "tochange");
+		EvaluateSubmissions.connect();
+		long ms = System.currentTimeMillis();
 		
+		for (int i = 0; i < 50; i++) {
+
+			EvaluateSubmissions.compileAndRun("JAVA", "import java.io.*;\r\n" + "import java.util.*;\r\n"
+					+ "import java.text.*;\r\n" + "import java.math.*;\r\n" + "import java.util.regex.*;\r\n" + "\r\n"
+					+ "public class Solution {\r\n" + "\r\n" + "	public static void main(String[] args) {\r\n"
+					+ "		Scanner in = new Scanner(System.in);\r\n" + "		int n = in.nextInt();\r\n"
+					+ "		int scores[] = new int[n];\r\n" + "		for (int i = 0; i < n; i++) {\r\n"
+					+ "			scores[i] = in.nextInt();\r\n" + "		}\r\n"
+					+ "		minimumDistances(n, scores);\r\n" + "	}\r\n" + "\r\n"
+					+ "	static void minimumDistances(int n, int array[]) {\r\n"
+					+ "		int min = Integer.MAX_VALUE;\r\n" + "		for (int i = 0; i < n-1; i++) {\r\n"
+					+ "			for(int j = i+1; j<n; j++) {\r\n" + "				if(array[i] == array[j]){\r\n"
+					+ "					min = Math.min(min,  Math.abs(i-j));\r\n" + "				}\r\n"
+					+ "			}\r\n" + "		}\r\n" + "		if(min==Integer.MAX_VALUE) {\r\n"
+					+ "			min = -1;\r\n" + "		}\r\n" + "		System.out.println(min);\r\n" + "	}\r\n"
+					+ "\r\n" + "\r\n" + "}", "input.txt", "nothing");
+
+			EvaluateSubmissions.compileAndRun("C++11", "#include <algorithm>\r\n" + "#include <cmath>\r\n"
+					+ "#include <bitset>\r\n" + "#include <cstdio>\r\n" + "#include <cstdlib>\r\n"
+					+ "#include <cstring>\r\n" + "#include <iostream>\r\n" + "#include <iomanip>\r\n"
+					+ "#include <map>\r\n" + "#include <set>\r\n" + "#include <sstream>\r\n" + "#include <string>\r\n"
+					+ "#include <unordered_map>\r\n" + "#include <unordered_set>\r\n" + "#include <vector>\r\n"
+					+ "#include <queue>\r\n" + "\r\n" + "using namespace std;\r\n" + "\r\n"
+					+ "#define all(x) x.begin(), x.end()\r\n" + "#define forn(i, n) for(int i = 0; i < (n); ++i)\r\n"
+					+ "#define debug(x) std::cerr << \"DEBUG: \" << #x << \" = \" << (x) << std::endl\r\n"
+					+ "#define mp make_pair\r\n" + "#define pb push_back\r\n"
+					+ "#define PATH \"C:\\\\Users\\\\Valentin\\\\Desktop\\\\\"\r\n" + "\r\n"
+					+ "template<class T> inline int sz(const T& x) { return (int) x.size(); }\r\n"
+					+ "template<class T> inline void mx(T& x, const T& y) { x = std::max(x, y); }\r\n"
+					+ "template<class T> inline void mn(T& x, const T& y) { x = std::min(x, y); }\r\n" + "\r\n"
+					+ "// SOLUTION BEGINS HERE\r\n" + "\r\n" + "void run() {\r\n" + "  int n;\r\n" + "  cin >> n;\r\n"
+					+ "  vector<pair<int, int>> a(n);\r\n" + "  forn (i, n) {\r\n" + "    cin >> a[i].first;\r\n"
+					+ "    a[i].second = i;\r\n" + "  }\r\n" + "  sort(all(a));\r\n" + "  int ans = n;\r\n"
+					+ "  forn (i, n - 1) {\r\n" + "    if (a[i].first == a[i + 1].first) {\r\n"
+					+ "      mn(ans, a[i + 1].second - a[i].second);\r\n" + "    }\r\n" + "  }\r\n"
+					+ "  if (ans == n) {\r\n" + "    ans = -1;\r\n" + "  }\r\n" + "  cout << ans << endl;\r\n" + "}\r\n"
+					+ "\r\n" + "\r\n" + "// SOLUTION ENDS HERE\r\n" + "\r\n" + "int main() {\r\n"
+					+ "  // freopen(PATH\"in.txt\", \"r\", stdin);\r\n" + "  std::ios_base::sync_with_stdio(false);\r\n"
+					+ "  std::cin.tie(nullptr);\r\n" + "  run();\r\n" + "  std::cout.flush();\r\n" + "  return 0;\r\n"
+					+ "}", "input.txt", "");
+
+			EvaluateSubmissions.compileAndRun("PYTHON",
+					"#!/bin/python\r\n" + "\r\n" + "import sys\r\n" + "\r\n" + "\r\n"
+							+ "n = int(raw_input().strip())\r\n" + "A = map(int,raw_input().strip().split(' '))\r\n"
+							+ "r=n+1\r\n" + "for i in range(n):\r\n" + "    for j in range(i+1,n):\r\n"
+							+ "        if A[j]==A[i]:\r\n" + "            r=min(j-i,r)\r\n" + "if r<=n:\r\n"
+							+ "    print r\r\n" + "else:\r\n" + "    print -1",
+					"input.txt", "");
+
+			EvaluateSubmissions.compileAndRun("C#",
+					"using System;\r\n" + "using System.Collections.Generic;\r\n" + "using System.IO;\r\n"
+							+ "using System.Linq;\r\n" + "class Solution {\r\n" + "\r\n"
+							+ "    static void Main(String[] args) {\r\n"
+							+ "        int n = Convert.ToInt32(Console.ReadLine());\r\n"
+							+ "        string[] A_temp = Console.ReadLine().Split(' ');\r\n"
+							+ "        int[] A = Array.ConvertAll(A_temp,Int32.Parse);\r\n" + "        \r\n"
+							+ "        int min = int.MaxValue;\r\n" + "        for(int i=0;i<n-1;i++){\r\n"
+							+ "            for(int j=i+1;j<n;j++){\r\n" + "                if(A[i]==A[j]){\r\n"
+							+ "                    int d = j-i;\r\n" + "                    if(d<min)min=d;\r\n"
+							+ "                }\r\n" + "            }\r\n" + "        }\r\n"
+							+ "        if(min==int.MaxValue) Console.WriteLine(-1);\r\n"
+							+ "        else Console.WriteLine(min);\r\n" + "    }\r\n" + "}",
+					"input.txt", "");
+		}
 		return "";
 	}
 
--- codeflex/src/main/java/pt/codeflex/evaluatesubmissions/EvaluateSubmissions.java
+++ codeflex/src/main/java/pt/codeflex/evaluatesubmissions/EvaluateSubmissions.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+import java.util.Queue;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.common.IOUtils;
@@ -15,19 +18,29 @@ import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
 import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.xfer.FileSystemFile;
+import pt.codeflex.controllers.api.DatabaseController;
+import pt.codeflex.models.Submissions;
+import pt.codeflex.repositories.SubmissionsRepository;
 
 public class EvaluateSubmissions {
 
 	private static final String SUBMISSIONS_FILE_DIR = System.getProperty("user.home") + File.separator + "Submissions";
 	private static final String SUBMISSIONS_SERVER = "/home/mbrito/Desktop/Submissions";
-	private long uniqueId = 1000;
+	private static long uniqueId = 1000;
+
+	private static Queue<Submissions> submissions;
+
+	@Autowired
+	private DatabaseController databaseController;
 
 	public EvaluateSubmissions() {
 		this.uniqueId++;
+		Iterable<Submissions> sub = databaseController.getAllSubmissions();
 	}
-	public SSHClient ssh = null;
 
-	public void connect() {
+	public static SSHClient ssh = null;
+
+	public static void connect() {
 
 		ssh = new SSHClient();
 		ssh.addHostKeyVerifier("33:02:cb:3b:13:b1:bd:fa:66:ff:29:96:ea:ff:dc:78");
@@ -47,20 +60,7 @@ public class EvaluateSubmissions {
 
 	}
 
-	public void createFile(String code, long uniqueId) {
-		PrintWriter writer = null;
-		try {
-			writer = new PrintWriter(SUBMISSIONS_FILE_DIR + "/Solution", "UTF-8");
-			writer.print(code);
-			writer.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void compileAndRun(String language, String code, String input, String output) {
+	public static void compileAndRun(String language, String code, String input, String output) {
 
 		String fileName = "Solution";
 		createFile(code, uniqueId);
@@ -79,20 +79,31 @@ public class EvaluateSubmissions {
 		}
 
 		uniqueId = uniqueId + 1;
-		String compileError = "cmp_error_" + fileName + "_" + uniqueId + ".txt";
-
+		String compileError = "error_" + uniqueId + ".txt";
+		String compileOutput = "output_" + uniqueId + ".txt";
 		String command = "cd Desktop/Submissions && ";
+		// TODO : add memory limit
 		switch (language) {
 		case "JAVA":
 			command += "javac " + fileName + ".java 2> " + compileError + " && cat " + input + " | timeout 3s java "
-					+ fileName + " > output_" + uniqueId + ".txt";
+					+ fileName + " > " + compileOutput + " &"; // && disown
 			break;
 		case "C++11":
-			command += "g++ -std=c++11 -o " + fileName + "_cpp_exec " + fileName + " && cat " + input + " | ./"
-					+ fileName + "_cpp_exec";
+			command += "g++ -std=c++11 -o " + fileName + "_exec_" + uniqueId + ".cpp " + fileName + " 2> "
+					+ compileError + " && cat " + input + " | timeout 2 ./" + fileName + "_exec_" + uniqueId + " 2> "
+					+ compileError + " > " + compileOutput + " &";
 			break;
-
+		case "PYTHON":
+			command += "cat " + input + " | timeout 10 python " + fileName + ".py 2> " + compileError + " > "
+					+ compileOutput + " &";
+			break;
+		case "C#":
+			command += "mcs -out:" + fileName + "_exec_" + uniqueId + " " + fileName + ".cs 2> " + compileError
+					+ " && cat " + input + " | mono " + fileName + "_exec_" + uniqueId + " 2> " + compileError + " > "
+					+ compileOutput + " &";
+			break;
 		default:
+			System.out.println("Language not found");
 			break;
 		}
 
@@ -100,6 +111,8 @@ public class EvaluateSubmissions {
 			System.out.println("COMANDO DE EXEC: " + command);
 			Command cmd = session.exec(command);
 			System.out.println(IOUtils.readFully(cmd.getInputStream()).toString());
+			cmd.close();
+			session.close();
 		} catch (ConnectionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -113,12 +126,61 @@ public class EvaluateSubmissions {
 
 	}
 
-	public void scp(String language, String src, String dest) {
+	public static void getFinishedJobs() { // TODO : review
+		Session session = null;
+		String output = "";
+		try {
+			session = ssh.startSession();
+		} catch (ConnectionException | TransportException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		String command = "cd Desktop/Submissions && cat output_*";
+		try {
+			Command cmd = session.exec(command);
+			output = IOUtils.readFully(cmd.getInputStream()).toString();
+		} catch (ConnectionException | TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(output);
+
+	}
+
+	public static void createFile(String code, long uniqueId) {
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(SUBMISSIONS_FILE_DIR + "/Solution", "UTF-8");
+			writer.print(code);
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void scp(String language, String src, String dest) {
 		// final String src = System.getProperty("user.home") + File.separator +
 		String suffix = "";
 		switch (language) {
 		case "JAVA":
 			suffix = ".java";
+			break;
+		case "C++11":
+			suffix = ".cpp";
+			break;
+		case "PYTHON":
+			suffix = ".py";
+			break;
+		case "C#":
+			suffix = ".cs";
+			break;
 		default:
 			break;
 		}
