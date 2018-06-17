f439ab49954209b0a1214a9f8e07c908720a790a
Miguel Brito <miguelfbrito11@gmail.com>
2018-06-17 17:57:55 +0100

    Added interface to add problems.

--- client/src/Problem/AddProblem/AddProblem.js
+++ client/src/Problem/AddProblem/AddProblem.js
@@ -53,13 +53,50 @@ class AddProblem extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
+            <div className="container add-problem">
+                <div className="row ">
                     <PathLink path={this.props.location.pathname} title="Add problem" />
-                    <div className="col-sm-2 add-problem-name">
+                    <div className="col-sm-2 add-problem-desc">
                         <h3>Name</h3>
                     </div>
-                    <div className="col-sm-10">
+                    <div className="col-sm-10 add-problem-textarea">
+                        <input type="text" id="input-problem-name" placeholder="Problem name" />
+                    </div>
+                    {this.state.description !== '' && draftToHtml(convertToRaw(this.state.description.getCurrentContent()))}
+                </div>
+
+                <div className="row info-section">
+                    <div className="col-sm-2 add-problem-desc">
+                        <h3>Problem Statement</h3>
+                        <p>Explain your problem here. Keep it simple and clean.</p>
+                    </div>
+                    <div className="col-sm-10 add-problem-textarea">
+                        <div className="text-editor-wrapper">
+                            <Editor
+                                id="description"
+                                wrapperClassName="demo-wrapper "
+                                editorClassName="demo-editor"
+                                editorState={this.state.description}
+                                //    editorState={this.state.description}
+                                onEditorStateChange={this.onEditorStateChange}
+                                toolbar={{
+                                    inline: { inDropdown: true },
+                                    list: { inDropdown: true },
+                                    textAlign: { inDropdown: true },
+                                    link: { inDropdown: true },
+                                    image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } }
+                                }}
+                            />
+                        </div>
+
+                    </div>
+                </div>
+                <div className="row info-section">
+                    <div className="col-sm-2 add-problem-desc">
+                        <h3>Constraints</h3>
+                        <p>Include all your constraints here, such as input and output size limitations.</p>
+                    </div>
+                    <div className="col-sm-10 add-problem-textarea">
                         <div className="text-editor-wrapper">
                             <Editor
                                 id="description"
@@ -78,7 +115,61 @@ class AddProblem extends React.Component {
                             />
                         </div>
                     </div>
-                    {this.state.description !== '' && divdraftToHtml(convertToRaw(this.state.description.getCurrentContent()))}
+                </div>
+                <div className="row info-section">
+                    <div className="col-sm-2 add-problem-desc">
+                        <h3>Input Format</h3>
+                        <p>Add your information regarding the expected input format.</p>
+                    </div>
+                    <div className="col-sm-10 add-problem-textarea">
+                        <div className="text-editor-wrapper">
+                            <Editor
+                                id="description"
+                                wrapperClassName="demo-wrapper "
+                                editorClassName="demo-editor"
+                                editorState={this.state.description}
+                                //    editorState={this.state.description}
+                                onEditorStateChange={this.onEditorStateChange}
+                                toolbar={{
+                                    inline: { inDropdown: true },
+                                    list: { inDropdown: true },
+                                    textAlign: { inDropdown: true },
+                                    link: { inDropdown: true },
+                                    image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } }
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="row info-section">
+                    <div className="col-sm-2 add-problem-desc">
+                        <h3>Output Format</h3>
+                        <p>Add your information regarding the expected output format.</p>
+                    </div>
+                    <div className="col-sm-10 add-problem-textarea">
+                        <div className="text-editor-wrapper">
+                            <Editor
+                                id="description"
+                                wrapperClassName="demo-wrapper "
+                                editorClassName="demo-editor"
+                                editorState={this.state.description}
+                                //    editorState={this.state.description}
+                                onEditorStateChange={this.onEditorStateChange}
+                                toolbar={{
+                                    inline: { inDropdown: true },
+                                    list: { inDropdown: true },
+                                    textAlign: { inDropdown: true },
+                                    link: { inDropdown: true },
+                                    image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: true } }
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-offset-2 col-sm-10 col-xs-12">
+                        <input type="button" className="btn btn-primary" name="" id="save-problem" value="Save problem"/>
+                    </div>
                 </div>
             </div>
         )
