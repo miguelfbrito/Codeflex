2e22377a40c0af1e45e11d254a948e0dd01cb551
Miguel Brito <miguelfbrito11@gmail.com>
2018-06-30 02:05:44 +0100

    Fix for maven build

--- server/src/main/java/pt/codeflex/controllers/DatabaseController.java
+++ server/src/main/java/pt/codeflex/controllers/DatabaseController.java
@@ -1,20 +1,14 @@
 package pt.codeflex.controllers;
 
-import static org.mockito.Mockito.doAnswer;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-import org.eclipse.jdt.internal.compiler.problem.ProblemSeverities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
